e7c62a3439bccfd827256ef4cc34114f7d7924c5
Philippe DELENTE <philippe.delente@gmail.com>
2024-04-18 14:35:05 +0200

    init eslint + change prettier

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { Home } from './pages/home.js'
 import { Events } from './pages/events/index.js'
 
 export const routes = {
+    '*': Home,
     '/': Home,
     '/events': Events,
     '/events/:id': Events,
