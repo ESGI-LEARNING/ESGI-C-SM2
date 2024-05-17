41992524b587ae990925ae49c7a237c7ac2a6f7d
tmeunier <theo.meunier41@gmail.com>
2024-05-18 01:33:02 +0200

    create router and render dom

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import { Contact } from './pages/contact.js'
+import { Home } from './pages/home.js'
+import { Events } from './pages/events/index.js'
+
+export const routes = {
+    '/': Home,
+    '/events': Events,
+    '/events/:id': Events,
+    '/contact': Contact
+}
