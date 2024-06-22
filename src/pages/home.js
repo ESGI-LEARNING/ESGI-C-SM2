eb7b001a3e5e46b4d655a5bbe9c5080f26047b11
Tinou95 <Quentin95170@hotmail.fr>
2024-06-22 13:19:36 +0200

    feat: mapSection

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,9 @@
 import { Component } from '../componennts/Component.js'
+import { Header } from '../components/header.js';
+import { Footer } from '../components/footer.js';
+import { HeroHome } from '../components/home/heroHome.js';
+import { evenementList } from '../components/home/evenementList.js';
+import { mapHome } from '../components/home/mapHome.js';
 
 export class Home extends Component {
     render() {
@@ -27,3 +32,15 @@ export class Home extends Component {
         }
     }
 }
+export const Home = {
+    type: 'div',
+    children: [
+        Header,
+        {
+            type: 'main',
+            children: [HeroHome, evenementList, mapHome],
+        },
+        Footer,
+        Footer,
+    ],
+};
